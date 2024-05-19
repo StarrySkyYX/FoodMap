8b3820ffc4b392ec77bd24fce9135e44ba47388f
StarrySkyYX <132223842+StarrySkyYX@users.noreply.github.com>
2024-05-19 20:34:11 +0800

    Create Store.kt
    class: add store

---
+++ app/src/main/java/com/example/foodmap/Store.kt
@@ -0,0 +1,14 @@
+package com.example.foodmap
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class Store (
+    @DrawableRes val pictureId: Int,
+    @DrawableRes val menuId: Int,
+    @StringRes val nameId: Int,
+    @StringRes val addressId: Int,
+//    @StringRes val longitude: Int,
+//    @StringRes val latitude: Int,
+)
+
