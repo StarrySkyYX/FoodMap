cac3cc97f0eca93599fdf761831ffacfeac766f5
StarrySkyYX <132223842+StarrySkyYX@users.noreply.github.com>
2024-05-19 20:33:49 +0800

    add library
     function: switch screen

--- app/src/main/java/com/example/foodmap/MainActivity.kt
+++ app/src/main/java/com/example/foodmap/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.example.foodmap.ui.FoodMapApp
 import com.example.foodmap.ui.theme.FoodMapTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +20,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FoodMapTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(navController, startDestination = "home") {
+                        composable("home") { FoodMapApp(navController) }
+                        composable(
+                            "detail/{nameId}/{addressId}/{imageId}/{menuId}",
+                            arguments = listOf(
+                                navArgument("nameId") { type = NavType.IntType },
+                                navArgument("addressId") { type = NavType.IntType },
+                                navArgument("imageId") { type = NavType.IntType },
+                                navArgument("menuId") { type = NavType.IntType },
+                            )
+                        ) { backStackEntry ->
+                            val nameId = backStackEntry.arguments?.getInt("nameId") ?: 0
+                            val addressId = backStackEntry.arguments?.getInt("addressId") ?: 0
+                            val imageId = backStackEntry.arguments?.getInt("imageId") ?: 0
+                            val menuId = backStackEntry.arguments?.getInt("menuId") ?: 0
+                            StoreApp(
+                                nameId = nameId,
+                                addressId = addressId,
+                                imageId = imageId,
+                                menuId = menuId,
+                                navController = navController
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FoodMapTheme {
-        Greeting("Android")
     }
 }
